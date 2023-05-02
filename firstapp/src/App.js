2126babbf68ba39f6b4fc810ae775ec4c3126601
Victor Cazacu <vcazacu@xperigo.com>
2023-05-02 00:51:30 -0400

    first commit

---
+++ firstapp/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+//import Heading from './Heading.js';
+
+function Heading() {
+    return (<h1>Hello World me1</h1>);
+}
+
+function App() {
+    return <Heading />;
+
+}
+
+export default App;
---
+++ firstapp/src/Heading.js
@@ -0,0 +1,5 @@
+function Heading() {
+    let title = "This is some heading text"
+    return (<h1>{title}</h1>);
+};
+export default Heading;
